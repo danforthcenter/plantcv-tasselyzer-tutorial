27d95f1efd461184cade724dd389851d7e3ff61d
sdkenney42 <128510721+sdkenney42@users.noreply.github.com>
2024-11-12 15:54:43 -0600

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,30 +1,18 @@
 import os
-import shutil
-import pytest
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
-project_root = os.getcwd()
-inputs_list = [
-    [project_root, 'index.ipynb']
-]
 
-
-# ##########################
-# Tests executing the notebook
-# ##########################
-@pytest.mark.parametrize('dir,notebook', inputs_list)
-def test_notebook(dir, notebook, tmpdir):
+def test_notebook(tmpdir):
+    """Test the notebook."""
     tmp = tmpdir.mkdir('sub')
-    # Change working directory
-    os.chdir(dir)
     # Open the notebook
-    with open(notebook, "r") as f:
+    with open("index.ipynb", "r") as f:
         nb = nbformat.read(f, as_version=4)
 
     # Process the notebook
     ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
-    ep.preprocess(nb, {"metadata": {"path": dir}})
+    ep.preprocess(nb, {"metadata": {"path": os.getcwd()}})
 
     # Save the executed notebook
     out_nb = os.path.join(tmp, "executed_notebook.ipynb")
